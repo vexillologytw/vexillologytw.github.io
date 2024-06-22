1318ecd49525f12bc552f971ad4ec526fd0606ec
samuel <samuelkoyunqi@gmail.com>
2024-06-22 20:05:58 +0800

    added windless flag display; optimized html components; fixed some issues

---
+++ js/load_html.js
@@ -0,0 +1,14 @@
+async function loadHtml() {
+    var loaders = document.querySelectorAll("[data-load]");
+    for (var loader of loaders) {
+        var loadTarget = loader.getAttribute("data-load");
+        for (var html of loadTarget.split(",")) {
+            try {
+                loader.innerHTML += await (await fetch(html)).text();
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+}
+loadHtml();
