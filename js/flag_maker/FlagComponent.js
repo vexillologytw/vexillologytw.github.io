4cbf82052354bac23a756a2a6a337045db592ac4
samuel <samuelkoyunqi@gmail.com>
2024-06-28 00:19:10 +0800

    optimized flag maker js

--- js/flag_maker/FlagComponent.js
+++ js/flag_maker/FlagComponent.js
@@ -20,12 +20,12 @@ class FlagComponent {
 
         createIdLoop:
         do {
-            var id = this.generateId(30);
+            var id = FlagComponent.generateId(30);
             if (safe-- < 0) {
                 error("無法建立元素");
                 break createIdLoop;
             }
-        } while (!this.validateId(id));
+        } while (!FlagComponent.validateId(id));
 
         this.id = id;
 
@@ -48,73 +48,152 @@ class FlagComponent {
         if (this.element == undefined) {
             return;
         }
-        document.addEventListener("mousedown", (e) => {
-            if (e.which != 1) return;
-            this.mouseDown = true;
-        });
         this.element.addEventListener("mousedown", (e) => {
             if (e.which != 1) return;
-            this.leftClickDown = true;
+            this.focusOn();
         });
         this.element.addEventListener("mousemove", (e) => {
             this.isHovering = true;
         });
         this.element.addEventListener("mouseleave", (e) => {
+            // maintain status if already doing functions
             this.isHovering = false;
         });
-        document.addEventListener("mousemove", (e) => {
-            var cursor = this.getCursorObBorder(e);
-            document.body.style.cursor = cursor;
-            if (!this.isMoving && this.isResizing !== "pointer" && this.isResizing !== "default") {
-                this.sizeElement(e, this.isResizing);
-            } else if (!this.isMoving && this.mouseDown && cursor !== "pointer" && cursor !== "default") {
-                this.isResizing = cursor;
-                this.sizeElement(e, cursor);
-            } else if (this.isMoving) {
-                this.moveElement(e);
-            } else if (this.isHovering && this.leftClickDown) {
-                this.isMoving = true;
-                this.moveElement(e);
+        document.addEventListener("mousedown", (e) => {
+            // only react if mouse is left click
+            if (e.which != 1) return;
+
+            var cursor = this.getCursorStyle(e);
+            if (cursor === "default") {
+                this.focusOff();
+                return;
             }
+            // remember where mouse downed
+            var p = this.element.getBoundingClientRect();
+            this.moveOffsetX = e.pageX - p.left;
+            this.moveOffsetY = e.pageY - p.top;
+
+            this.isMouseDown = true;
+        });
+        document.addEventListener("mousemove", (e) => {
+            if (this.isFocus !== true) return;
+            this.processCursorEvent(e);
         });
         document.addEventListener("mouseup", (e) => {
+            if (this.isFocus !== true) return;
+            this.isMouseDown = false;
             this.isMoving = false;
-            this.leftClickDown = false;
-            this.mouseDown = false;
-            this.isResizing = "default";
-            this.fromX = undefined;
-            this.fromY = undefined;
+            this.isSizing = false;
+            
+            var p = this.element.getBoundingClientRect();
+            if (Math.abs(p.left - p.right) <= 10) {
+                this.info("物件寬度不得為 0");
+                this.element.style.width = `10px`;
+            }
+            if (Math.abs(p.top - p.bottom) <= 10) {
+                this.info("物件長度不得為 0");
+                this.element.style.height = `10px`;
+            }
         });
+    }
+
+    processCursorEvent(e) {
+        var cursor;
+        // if cursor is already doing moving or sizing, remain style
+        if (this.isMoving === true || this.isSizing === true) {
+            cursor = document.body.style.cursor;
+        // if cursor is hovering on element, element's border, or neither, update style
+        } else {
+            cursor = this.getCursorStyle(e);
+            document.body.style.cursor = cursor;
+        }
+        // event only happens when mouse is downed
+        if (this.isMouseDown !== true) return;
+        switch (cursor) {
+            case "pointer": // dragging if pointing at element
+                this.isMoving = true;
+                this.moveElement(e);
+                break;
+            case "default": // do nothing at default
+                this.moveOffsetX = undefined;
+                this.moveOffsetY = undefined;
+                break;
+            default:        // resize element if pointing at corners (neither point nor default)
+                this.isSizing = true;
+                this.sizeElement(e, cursor);
+                break;
+        }
     }
 
     sizeElement(e, cursor) {
         var element = this.getElement();
         var p = element.getBoundingClientRect();
+
         switch (cursor) {
             case "nw-resize":
+                this.sizeNorth(element, e, p);
+                this.sizeWest(element, e, p);
                 break;
             case "ne-resize":
+                this.sizeNorth(element, e, p);
+                this.sizeEast(element, e, p);
                 break;
             case "sw-resize":
+                this.sizeSouth(element, e, p);
+                this.sizeWest(element, e, p);
                 break;
             case "se-resize":
-                element.style.height = `${e.pageY - p.top}px`;
-                element.style.width = `${e.pageX - p.left}px`;
+                this.sizeEast(element, e, p);
+                this.sizeSouth(element, e, p);
                 break;
             case "n-resize":
+                this.sizeNorth(element, e, p);
                 break;
             case "s-resize":
-                element.style.height = `${e.pageY - p.top}px`;
+                this.sizeSouth(element, e, p);
                 break;
             case "w-resize":
+                this.sizeWest(element, e, p);
                 break;
             case "e-resize":
-                element.style.width = `${e.pageX - p.left}px`;
+                this.sizeEast(element, e, p);
                 break;
         }
     }
 
-    getCursorObBorder(e) {
+    sizeNorth(element, e, p) {
+        if ((p.bottom - e.pageY) > 10) {
+            element.style.top = `${e.pageY}px`;
+            element.style.height = `${p.bottom - e.pageY}px`;
+        }
+    }
+
+    sizeSouth(element, e, p) {
+        element.style.height = `${e.pageY - p.top}px`;
+    }
+
+    sizeEast(element, e, p) {
+        if ((e.pageX - p.left) > 10) {
+            element.style.width = `${e.pageX - p.left}px`;
+        }
+    }
+
+    sizeWest(element, e, p) {
+        if ((p.right - e.pageX) > 10) {
+            element.style.left = `${e.pageX}px`;
+            element.style.width = `${p.right - e.pageX}px`;
+        }
+    }
+
+    moveElement(e) {
+        var moveToX = e.pageX - this.moveOffsetX;
+        var moveToY = e.pageY - this.moveOffsetY;
+        
+        this.element.style.left = `${moveToX}px`;
+        this.element.style.top = `${moveToY}px`;
+    }
+
+    getCursorStyle(e) {
         var p = this.element.getBoundingClientRect();
         var isNearTop = this.isNearTop(e, p);
         var isNearBottom = this.isNearBottom(e, p);
@@ -123,21 +202,21 @@ class FlagComponent {
 
         if (isNearTop && isNearLeft) {
             return "nw-resize";
-        } else if (isNearTop && isNearRight) {
+        } else if (isNearTop === true && isNearRight === true) {
             return "ne-resize";
-        } else if (isNearBottom && isNearLeft) {
+        } else if (isNearBottom === true && isNearLeft === true) {
             return "sw-resize";
-        } else if (isNearBottom && isNearRight) {
+        } else if (isNearBottom === true && isNearRight === true) {
             return "se-resize";
-        } else if (isNearTop) {
+        } else if (isNearTop === true) {
             return "n-resize";
-        } else if (isNearBottom) {
+        } else if (isNearBottom === true) {
             return "s-resize";
-        } else if (isNearLeft) {
+        } else if (isNearLeft === true) {
             return "w-resize";
-        } else if (isNearRight) {
+        } else if (isNearRight === true) {
             return "e-resize";
-        } else if (this.isHovering) {
+        } else if (this.isHovering === true) {
             return "pointer";
         }
         return "default";
@@ -155,11 +234,9 @@ class FlagComponent {
     isNearBottom(e, p) {
         if (e.pageX >= p.left -10 && e.pageX <= p.right +10) {
             if (Math.abs(e.pageY - p.bottom) <= 10) {
-                document.body.style.cursor = "s-resize";
                 return true;
             }
         }
-        document.body.style.cursor = "pointer";
         return false;
     }
 
@@ -181,21 +258,6 @@ class FlagComponent {
         return false;
     }
 
-    moveElement(e, fromX, fromY) {
-        var p = this.element.getBoundingClientRect();
-
-        if (this.fromX == undefined && this.fromY == undefined) {
-            this.fromX = e.pageX - p.left; // how far away the cursor is to the element
-            this.fromY = e.pageY - p.top;
-        }
-        var moveToX = e.pageX - this.fromX;
-        var moveToY = e.pageY - this.fromY;
-        
-
-        this.element.style.left = `${moveToX}px`;
-        this.element.style.top = `${moveToY}px`;
-    }
-
     getElement() {
         if (this.id == undefined) {
             error('查無元素');
@@ -204,14 +266,26 @@ class FlagComponent {
         return document.getElementById(this.id);
     }
 
-    validateId(id) {
+    focusOn() {
+        this.info("focus on");
+        this.getElement().style.boxShadow = "10px 10px 20px 0px #000000";
+        this.isFocus = true;
+    }
+
+    focusOff() {
+        this.info("focus off");
+        this.getElement().style.boxShadow = null;
+        this.isFocus = false;
+    }
+
+    static validateId(id) {
         if (document.getElementById(id)) {
             return false;
         }
         return true;
     }
 
-    generateId(length) {
+    static generateId(length) {
         let result = '';
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-';
         const charactersLength = characters.length;
