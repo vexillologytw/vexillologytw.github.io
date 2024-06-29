9eb1d5c2397d848349d867c5cda0d3a8af78e256
samuel <samuelkoyunqi@gmail.com>
2024-06-29 23:35:06 +0800

    added option panel

--- js/flag_maker/FlagComponent.js
+++ js/flag_maker/FlagComponent.js
@@ -4,7 +4,7 @@ class FlagComponent {
         this.params = {
             width: "10em",
             height: "10em",
-            backgroundColor: "#bc002e",
+            backgroundColor: FlagComponent.generateColor(),
             placement: undefined
         }
         if (params != undefined) {
@@ -40,6 +40,7 @@ class FlagComponent {
         if (placement != undefined) {
             placement.appendChild(element);
         }
+        this.setToTopZIndex();
         this.setEvents();
     }
 
@@ -70,8 +71,8 @@ class FlagComponent {
             }
             // remember where mouse downed
             var p = this.element.getBoundingClientRect();
-            this.moveOffsetX = e.pageX - p.left;
-            this.moveOffsetY = e.pageY - p.top;
+            this.moveOffsetX = e.pageX - this.elePos(p).left;
+            this.moveOffsetY = e.pageY - this.elePos(p).top;
 
             this.isMouseDown = true;
         });
@@ -86,11 +87,11 @@ class FlagComponent {
             this.isSizing = false;
             
             var p = this.element.getBoundingClientRect();
-            if (Math.abs(p.left - p.right) <= 10) {
+            if (Math.abs(this.elePos(p).left - this.elePos(p).right) <= 10) {
                 this.info("物件寬度不得為 0");
                 this.element.style.width = `10px`;
             }
-            if (Math.abs(p.top - p.bottom) <= 10) {
+            if (Math.abs(this.elePos(p).top - this.elePos(p).bottom) <= 10) {
                 this.info("物件長度不得為 0");
                 this.element.style.height = `10px`;
             }
@@ -123,6 +124,23 @@ class FlagComponent {
                 this.sizeElement(e, cursor);
                 break;
         }
+        this.elementParamToPanel();
+    }
+
+    elementParamToPanel() {
+        if (this.isFocus !== true) return;
+
+        var element = this.getElement();
+        var p = element.getBoundingClientRect();
+
+        document.querySelector("#component-x-input").value = this.elePos(p).top;
+        document.querySelector("#component-y-input").value = this.elePos(p).left;
+        document.querySelector("#component-width-input").value = element.offsetWidth;
+        document.querySelector("#component-height-input").value = element.offsetHeight;
+    }
+
+    panelParamToElement() {
+
     }
 
     sizeElement(e, cursor) {
@@ -162,26 +180,26 @@ class FlagComponent {
     }
 
     sizeNorth(element, e, p) {
-        if ((p.bottom - e.pageY) > 10) {
+        if ((this.elePos(p).bottom - e.pageY) > 10) {
             element.style.top = `${e.pageY}px`;
-            element.style.height = `${p.bottom - e.pageY}px`;
+            element.style.height = `${this.elePos(p).bottom - e.pageY}px`;
         }
     }
 
     sizeSouth(element, e, p) {
-        element.style.height = `${e.pageY - p.top}px`;
+        element.style.height = `${e.pageY - this.elePos(p).top}px`;
     }
 
     sizeEast(element, e, p) {
-        if ((e.pageX - p.left) > 10) {
-            element.style.width = `${e.pageX - p.left}px`;
+        if ((e.pageX - this.elePos(p).left) > 10) {
+            element.style.width = `${e.pageX - this.elePos(p).left}px`;
         }
     }
 
     sizeWest(element, e, p) {
-        if ((p.right - e.pageX) > 10) {
+        if ((this.elePos(p).right - e.pageX) > 10) {
             element.style.left = `${e.pageX}px`;
-            element.style.width = `${p.right - e.pageX}px`;
+            element.style.width = `${this.elePos(p).right - e.pageX}px`;
         }
     }
 
@@ -223,8 +241,8 @@ class FlagComponent {
     }
 
     isNearTop(e, p) {
-        if (e.pageX >= p.left -10 && e.pageX <= p.right +10) {
-            if (Math.abs(e.pageY - p.top) <= 10) {
+        if (e.pageX >= this.elePos(p).left -10 && e.pageX <= this.elePos(p).right +10) {
+            if (Math.abs(e.pageY - this.elePos(p).top) <= 10) {
                 return true;
             }
         }
@@ -232,8 +250,8 @@ class FlagComponent {
     }
 
     isNearBottom(e, p) {
-        if (e.pageX >= p.left -10 && e.pageX <= p.right +10) {
-            if (Math.abs(e.pageY - p.bottom) <= 10) {
+        if (e.pageX >= this.elePos(p).left -10 && e.pageX <= this.elePos(p).right +10) {
+            if (Math.abs(e.pageY - this.elePos(p).bottom) <= 10) {
                 return true;
             }
         }
@@ -241,8 +259,8 @@ class FlagComponent {
     }
 
     isNearLeft(e, p) {
-        if (e.pageY >= p.top -10 && e.pageY <= p.bottom +10) {
-            if (Math.abs(e.pageX - p.left) <= 10) {
+        if (e.pageY >= this.elePos(p).top -10 && e.pageY <= this.elePos(p).bottom +10) {
+            if (Math.abs(e.pageX - this.elePos(p).left) <= 10) {
                 return true;
             }
         }
@@ -250,12 +268,28 @@ class FlagComponent {
     }
 
     isNearRight(e, p) {
-        if (e.pageY >= p.top -10 && e.pageY <= p.bottom +10) {
-            if (Math.abs(e.pageX - p.right) <= 10) {
+        if (e.pageY >= this.elePos(p).top -10 && e.pageY <= this.elePos(p).bottom +10) {
+            if (Math.abs(e.pageX - this.elePos(p).right) <= 10) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * 部分功能是利用計算滑鼠位於畫面的位置與物件與 doc 的相對位置來實現，
+     * 因此在滾動畫面時，滑鼠的位置會無法對上物件的正確位置，
+     * 因此需要另外加上 window.scrollX / Y 的誤差值。
+     * @param {*} p getBoundingClientRect() 的值
+     * @returns 
+     */
+    elePos(p) {
+        return {
+            top: p.top + window.scrollY,
+            bottom: p.bottom + window.scrollY,
+            left: p.left + window.scrollX,
+            right: p.right + window.scrollX
+        }
     }
 
     getElement() {
@@ -267,15 +301,34 @@ class FlagComponent {
     }
 
     focusOn() {
-        this.info("focus on");
-        this.getElement().style.boxShadow = "10px 10px 20px 0px #000000";
-        this.isFocus = true;
+        if (focusId === undefined) {
+            this.setToTopZIndex();
+            this.getElement().style.boxShadow = "10px 10px 30px 0px #787878";
+            this.isFocus = true;
+            focusId = this.id;
+        }
     }
 
     focusOff() {
-        this.info("focus off");
-        this.getElement().style.boxShadow = null;
-        this.isFocus = false;
+        if (focusId === this.id) {
+            this.getElement().style.boxShadow = null;
+            this.isFocus = false;
+            focusId = undefined;
+        }
+    }
+
+    setToTopZIndex() {
+        var highest_index = 0;
+
+        var elements = document.getElementsByTagName("*");
+        var highest_index = 0;
+
+        for (var i = 0; i < elements.length - 1; i++) {
+            if (parseInt(elements[i].style.zIndex) > highest_index) {
+                highest_index = parseInt(elements[i].style.zIndex);
+            }
+        }
+        this.getElement().style.zIndex = highest_index +1;
     }
 
     static validateId(id) {
@@ -283,6 +336,18 @@ class FlagComponent {
             return false;
         }
         return true;
+    }
+
+    static generateColor() {
+        let result = '#';
+        const characters = 'ABCDEF0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < 6) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return result;
     }
 
     static generateId(length) {
